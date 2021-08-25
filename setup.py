c9aa7269c3a3d9a618dbf41aac8e23649be64d48
Swaroop P <iamswaroopp@gmail.com>
2021-08-25 21:10:37 +0530

    bump version 0.0.1a8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from setuptools import setup
 from setuptools import setup, find_packages
 
-version = '0.0.1a7'
+version = '0.0.1a8'
 
 description = Path(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                 'README.md')).read_text(encoding='utf-8').strip()
